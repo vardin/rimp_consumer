dd36f053a3497aaacbd667a15113ac4390bce205
Starcom <Starcom@DESKTOP-SE69V6U>
2017-11-06 20:13:37 +0900

    firt_commit

---
+++ src/main/java/cuda_matrix/cuda_matrix.java
@@ -0,0 +1,153 @@
+package cuda_matrix;
+
+import static jcuda.driver.JCudaDriver.cuCtxCreate;
+import static jcuda.driver.JCudaDriver.cuCtxSynchronize;
+import static jcuda.driver.JCudaDriver.cuDeviceGet;
+import static jcuda.driver.JCudaDriver.cuInit;
+import static jcuda.driver.JCudaDriver.cuLaunchKernel;
+import static jcuda.driver.JCudaDriver.cuMemAlloc;
+import static jcuda.driver.JCudaDriver.cuMemFree;
+import static jcuda.driver.JCudaDriver.cuMemcpyDtoH;
+import static jcuda.driver.JCudaDriver.cuMemcpyHtoD;
+import static jcuda.driver.JCudaDriver.cuModuleGetFunction;
+import static jcuda.driver.JCudaDriver.cuModuleLoad;
+
+import jcuda.Pointer;
+import jcuda.Sizeof;
+import jcuda.driver.CUcontext;
+import jcuda.driver.CUdevice;
+import jcuda.driver.CUdeviceptr;
+import jcuda.driver.CUfunction;
+import jcuda.driver.CUmodule;
+import jcuda.driver.JCudaDriver;
+import JCudaSamplesUtils.*;
+
+public class cuda_matrix {
+		
+	int width;
+	int numElemnets;
+	CUfunction function;
+	int blockSizeX;
+	int gridSizeX;
+	Pointer kernelParameters;
+	byte hostInputA[];
+	byte hostInputB[];
+	byte hostOutput[];
+	CUdeviceptr deviceInputA;
+	CUdeviceptr deviceInputB;
+	CUdeviceptr deviceOutput;
+	
+	public cuda_matrix(int input_width){
+	
+		
+		// Enable exceptions and omit all subsequent error checks
+		JCudaDriver.setExceptionsEnabled(true);
+
+		// Create the PTX file by calling the NVCC
+		String ptxFileName = JCudaSamplesUtils
+				.preparePtxFile("src/main/resources/kernels/JCudaVectorMatrixMultiplication.cu");
+
+		// Initialize the driver and create a context for the first device.
+		cuInit(0);
+		
+		
+		CUdevice device = new CUdevice();
+		cuDeviceGet(device, 0);
+		CUcontext context = new CUcontext();
+		cuCtxCreate(context, 0, device);
+
+		// Load the ptx file.
+		CUmodule module = new CUmodule();
+		cuModuleLoad(module, ptxFileName);
+
+		// Obtain a function pointer to the "add" function.
+		function = new CUfunction();
+		cuModuleGetFunction(function, module, "multiplication");
+
+		this.width = input_width*input_width;
+		numElemnets = (width) * (width);
+		
+		System.out.println("constructor has finished! ");
+		
+	}
+	
+	public void prepare_cuda_memory(byte[] input)
+	{
+		// Allocate and fill the host input data
+	
+		hostInputA = new byte[numElemnets];
+		hostInputB = new byte[numElemnets];
+		hostOutput = new byte[numElemnets];
+			
+		
+		for (int i = 0; i < numElemnets; i++) {
+					hostInputA[i] = (byte) (i%128);
+					hostInputB[i] = (byte)(i%128);
+				}
+			
+				hostInputA = input.clone();
+				hostInputB = input.clone();
+		
+				System.out.println("hostInputA[0] ="+hostInputA[0]);
+				System.out.println(hostInputA[0]);
+				System.out.println(hostInputA[1]);
+				
+				// Allocate the device input data, and copy the
+				// host input data to the device
+				
+		//		hostInputA = InputA;
+	//			hostInputB = InputA;
+				
+		
+				deviceInputA = new CUdeviceptr();
+				cuMemAlloc(deviceInputA, numElemnets * Sizeof.BYTE);
+				deviceInputB = new CUdeviceptr();
+				cuMemAlloc(deviceInputB, numElemnets * Sizeof.BYTE);
+				deviceOutput = new CUdeviceptr();
+				cuMemAlloc(deviceOutput, numElemnets * Sizeof.BYTE);
+		
+				System.out.println("before Memcpy!");
+				
+				cuMemcpyHtoD(deviceInputA, Pointer.to(hostInputA), numElemnets * Sizeof.BYTE);
+
+				cuMemcpyHtoD(deviceInputB, Pointer.to(hostInputB), numElemnets * Sizeof.BYTE);
+
+				// Allocate device output memory
+
+				System.out.println("after Memcpy!");
+				
+				// Set up the kernel parameters: A pointer to an array
+				// of pointers which point to the actual values.
+
+			kernelParameters = Pointer.to(Pointer.to(deviceInputA),
+						Pointer.to(deviceInputB), Pointer.to(deviceOutput),Pointer.to(new int[] { width }));
+				// Call the kernel function.
+				blockSizeX = width; //the number of thread
+				// int gridSizeX = (int)Math.ceil((double)numElements / blockSizeX);
+				gridSizeX = width;	 //the number of block
+			
+				cuLaunchKernel(function, gridSizeX, gridSizeX, 1, // Grid dimension //number of block
+						blockSizeX, blockSizeX, 1, // Block dimension //number of thread
+						0, null, // Shared memory size and stream
+						kernelParameters, null // Kernel- and extra parameters
+				);
+				cuCtxSynchronize();
+				// Allocate host output memory and copy the device output
+				// to the host.
+				cuMemcpyDtoH(Pointer.to(hostOutput), deviceOutput, numElemnets * Sizeof.BYTE);
+				
+				
+				System.out.println("result hostOutput[0] = "+hostOutput[0]);
+				System.out.println("cuda has finished!");
+	}
+	
+	public void cudaCleanUp(){
+		// Clean up.
+		cuMemFree(deviceInputA);
+		cuMemFree(deviceInputB);
+		cuMemFree(deviceOutput);
+		
+	}
+	
+	
+}
---
+++ src/main/java/matrix_consumer/matrix_consumer.java
@@ -0,0 +1,79 @@
+package matrix_consumer;
+
+import cuda_matrix.cuda_matrix;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import java.util.stream.IntStream;
+import javax.swing.plaf.synth.SynthSeparatorUI;
+import kafka.consumer.Consumer;
+import kafka.consumer.ConsumerConfig;
+import kafka.consumer.KafkaStream;
+import kafka.javaapi.consumer.ConsumerConnector;
+import kafka.message.MessageAndMetadata;
+
+public class matrix_consumer {
+
+	private static final String TOPIC = "supercom";
+	private static final int NUM_THREADS = 2;
+
+	// static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
+	public static void main(String[] args) throws Exception {
+
+		// System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		Properties props = new Properties();
+		props.put("group.id", "super-group");
+		props.put("zookeeper.connect", "163.152.174.73:2182");
+		props.put("auto.commit.interval.ms", "100");
+		ConsumerConfig consumerConfig = new ConsumerConfig(props);
+		ConsumerConnector consumer = Consumer.createJavaConsumerConnector(consumerConfig);
+		Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
+		topicCountMap.put(TOPIC, NUM_THREADS);
+		Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap = consumer.createMessageStreams(topicCountMap);
+		List<KafkaStream<byte[], byte[]>> streams = consumerMap.get(TOPIC);
+		ExecutorService executor = Executors.newFixedThreadPool(NUM_THREADS);
+
+		System.out.println("partial success!");
+
+		for (final KafkaStream<byte[], byte[]> stream : streams) {   //for 문이 2개 들어가 있는게 문제~ thread pool을 1로 수정
+			System.out.println("for start! 1");
+			// MyFrame frame = new MyFrame(); 
+			
+				Runnable runnable = new Runnable() {
+					public void run() {
+						
+					for (final MessageAndMetadata<byte[], byte[]> messageAndMetadata : stream) {
+						System.out.println("for start! 2 thread name : "+ Thread.currentThread().getName());				
+						cuda_matrix jcuda_matrix = new cuda_matrix(23);		
+						System.out.println("for start! 3"); 
+						byte[] test = messageAndMetadata.message();
+						
+						System.out.println(test.length);
+						jcuda_matrix.prepare_cuda_memory(test);
+				
+						// Mat data = new Mat(480, 640, CvType.CV_8UC3);
+						// data.put(0, 0, test);
+						// frame.setVisible(true);
+						// frame.render(data);
+						
+						System.out.println("one_complete! ");
+					
+			}
+					}
+				};
+				executor.execute(runnable);
+		}
+	
+		Thread.sleep(100000);
+		System.out.println("empty topic!!");
+		consumer.shutdown();
+		executor.shutdown();
+	}
+
+}
