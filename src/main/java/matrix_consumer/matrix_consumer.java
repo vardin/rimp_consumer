94eb2262d3c242ea10443d53cb907574a1b7a47c
Starcom <Starcom@DESKTOP-SE69V6U>
2017-12-18 17:00:57 +0900

    args topic

--- src/main/java/matrix_consumer/matrix_consumer.java
+++ src/main/java/matrix_consumer/matrix_consumer.java
@@ -20,12 +20,13 @@ import kafka.message.MessageAndMetadata;
 
 public class matrix_consumer {
 
-	private static final String TOPIC = "supercom";
+	private static String TOPIC;
 	private static final int NUM_THREADS = 1;
 
 	// static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
 	public static void main(String[] args) throws Exception {
-
+		
+		TOPIC = args[0];
 		// System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		Properties props = new Properties();
 		props.put("group.id", "super-group");
