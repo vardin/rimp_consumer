d3aec3fa25298380bbc5d3988cc496bb4caff355
Starcom <Starcom@DESKTOP-SE69V6U>
2017-12-21 02:33:06 +0900

    cpu2500

--- src/main/java/matrix_consumer/matrix_consumer.java
+++ src/main/java/matrix_consumer/matrix_consumer.java
@@ -21,7 +21,7 @@ import kafka.message.MessageAndMetadata;
 
 public class matrix_consumer {
 
-	private static final String TOPIC = "2500";
+	private static final String TOPIC = "cpu2500";
 	private static final int NUM_THREADS = 1;
 
 	// static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
