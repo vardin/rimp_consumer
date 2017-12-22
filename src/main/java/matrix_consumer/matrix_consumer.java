3244bd178be17064e32572506250c2925cce2114
Starcom <Starcom@DESKTOP-SE69V6U>
2017-12-23 01:41:13 +0900

    3partition

--- src/main/java/matrix_consumer/matrix_consumer.java
+++ src/main/java/matrix_consumer/matrix_consumer.java
@@ -21,7 +21,7 @@ import kafka.message.MessageAndMetadata;
 
 public class matrix_consumer {
 
-	private static final String TOPIC = "3partition";
+	private static final String TOPIC = "3partition1";
 	private static final int NUM_THREADS = 1;
 
 	// static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
