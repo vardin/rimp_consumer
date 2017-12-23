7f447ebe38c44a3b3d88811a7278505ddd20f875
Starcom <Starcom@DESKTOP-SE69V6U>
2017-12-23 17:28:49 +0900

    supercom3

--- src/main/java/matrix_consumer/matrix_consumer.java
+++ src/main/java/matrix_consumer/matrix_consumer.java
@@ -21,7 +21,7 @@ import kafka.message.MessageAndMetadata;
 
 public class matrix_consumer {
 
-	private static final String TOPIC = "supercom2";
+	private static final String TOPIC = "supercom3";
 	private static final int NUM_THREADS = 1;
 
 	// static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
